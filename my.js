df430d69a250fae691f18fd29994b9f36d237232
KhaiHung1707 <khaihung170700@gmail.com>
2021-12-04 17:26:56 +0700

    bai tap tong hai so nguyen

---
+++ my.js
@@ -0,0 +1,6 @@
+function tongHaiSoNguyen(){
+    let x = +document.getElementById("socong").value
+    let y = +document.getElementById("soduoccong").value
+    let sum = x+y ;
+    document.write("Tổng Hai Số Bằng "+ sum )
+}
